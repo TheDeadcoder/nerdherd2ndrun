7016a599da3f805e541df63f40ea35110f0f3696
Labid Al Nahiyan <68603298+labid-al-nahiyan@users.noreply.github.com>
2024-01-29 11:07:05 +0600

    fix library structure

---
+++ src/lib/OverlayPdf.svelte
@@ -0,0 +1,27 @@
+<script>
+	import PdfViewer from "svelte-pdf";
+    import {clickOutside} from '../stores/clickOutside'
+    import {isOverlayPdf} from '../stores/OverlayPdf.js'
+
+    export let url;
+
+    let buttons = ["navigation", "zoom", "rotate", "download", "timeInfo", "pageInfo"]
+
+    const handleClickOutside = (e)=>{
+         isOverlayPdf.set(false);
+    }
+
+</script>
+
+<div class="w-screen h-screen fixed top-0 left-0 flex justify-center items-center">
+    <div class="w-1/2 h-3/4" use:clickOutside on:click_outside={handleClickOutside} >
+         <iframe src={url} width="100%" height="100%" title=""></iframe>
+         <!-- <div style="width: 50%;" use:clickOutside on:click_outside={handleClickOutside} >
+              <PdfViewer scale=1 showBorder=False url={book.url}/> -->
+             <!-- <PdfViewer scale=0.5 showBorder=False url={url}
+                        showButtons = {buttons}
+             />
+         </div>       --> 
+    </div> 
+
+</div>
