88c1bc2362775c0f586b65a166df8ab32bd186a7
Labid Al Nahiyan <68603298+labid-al-nahiyan@users.noreply.github.com>
2024-02-07 21:05:42 +0600

    navbar design huge change

--- src/routes/accessType/+page.svelte
+++ src/routes/accessType/+page.svelte
@@ -26,146 +26,156 @@
 	});
 </script>
 
-<nav class="fixed top-0 z-50 w-full py-6 backdrop-blur-md">
-	<div class="flex justify-between mx-6">
-		<div class="flex">
-			<img
-				src="https://dxpcgmtdvyvcxbaffqmt.supabase.co/storage/v1/object/public/demo/GeekGlasses.png"
-				class="transform transition duration-300 hover:rotate-12"
-				alt="title"
-				width={60}
-			/>
-			<span class="company-name text-2xl font-extrabold">NerD</span><span
-				class="company-name white-text text-2xl font-extrabold">Herd</span
-			>
+<div class="accessTypeContainer">
+	<nav class=" w-full py-6">
+		<div class="flex justify-between mx-6">
+			<div class="flex">
+				<img
+					src="https://dxpcgmtdvyvcxbaffqmt.supabase.co/storage/v1/object/public/demo/GeekGlasses.png"
+					class="transform transition duration-300 hover:rotate-12"
+					alt="title"
+					width={60}
+				/>
+				<span class="company-name text-2xl font-extrabold">NerD</span><span
+					class="company-name white-text text-2xl font-extrabold">Herd</span
+				>
+			</div>
+	
+			<div >
+				<!-- <LightSwitch /> -->
+				<a class="btn btn-sm variant-ghost-surface" href="/about" target="_blank" rel="noreferrer">
+					About us
+				</a>
+			</div>
 		</div>
-
-		<div class="flex space-x-3">
-			<LightSwitch />
-			<a class="btn btn-sm variant-ghost-surface" href="/about" target="_blank" rel="noreferrer">
-				About us
-			</a>
-		</div>
-	</div>
-</nav>
-<section class="mt-28">
-	{#if isLoading}
-		<div class="flex space-x-2 justify-center items-center h-screen dark:invert">
-			<div class="h-8 w-8 bg-black rounded-full animate-bounce [animation-delay:-0.3s]"></div>
-			<div class="h-8 w-8 bg-black rounded-full animate-bounce [animation-delay:-0.15s]"></div>
-			<div class="h-8 w-8 bg-black rounded-full animate-bounce"></div>
-		</div>
-	{:else}
-		<div class=" mx-auto flex  justify-center items-center space-x-8 mb-1">
-			<!-- {#if isLearner === 'ON'} -->
-				<div class="flex flex-col space-y-6 w-1/3 items-center justify-center ">
-					<div class="text-center flex flex-col items-center">
-						<div class="flex card shadow-2xl rounded-xl overflow-hidden max-w-4xl">
-							<!-- Photo on the left side of the card -->
-
-							<!-- Previous Card Content -->
-							<div class="w-full p-8 flex flex-col items-center relative">
-								<!-- <div class="absolute right-2 ">
-									<Switch bind:value={isLearner} fontSize={12} label="Switch to Learner" design="slider" />
-								</div> -->
-								<div class="flex justify-center w-1/2">
-									<img
-										src="https://dxpcgmtdvyvcxbaffqmt.supabase.co/storage/v1/object/public/demo/teacher-removebg-preview.png"
-										alt="Trainer"
-										class="object-contain object-center mb-6"
-									/>
-								</div>
-								<div class="flex flex-col space-y-9">
-									<div class="text-3xl text-center font-bold mb-6">As a Trainer</div>
-								</div>
-								<div>
-									<p class="text-l text-center">
-										Create your own class to teach enthusiast individuals.
-										<br />
-										Set the goals. Maintain the resources. Take Video classroom.
-										<br />Create Question for Community purpose.
-									</p>
-								</div>
-
-								<div class="w-[100%] mt-5">
-									<button
-										type="submit"
-										class=" btn font-bold text-xl p-4 bg-[#77B8De] rounded-xl shadow-md hover:bg-[#619ecf] hover:text-[21px] hover:shadow-lg w-1/2"
-										on:click={handleSignInTrainer}
-									>
-										Sign In
-									</button>
-								</div>
-
-								<div class="flex flex-col items-center justify-between mt-3">
-									<p>
-										Don't have account? <a  href="/trainersignup" target="_blank" class="font-medium"><span class="hover-underline-animation">Sign up</span></a>
-									</p>
+	</nav>
+	<section >
+		{#if isLoading}
+			<div class="flex space-x-2 justify-center items-center h-screen dark:invert">
+				<div class="h-8 w-8 bg-black rounded-full animate-bounce [animation-delay:-0.3s]"></div>
+				<div class="h-8 w-8 bg-black rounded-full animate-bounce [animation-delay:-0.15s]"></div>
+				<div class="h-8 w-8 bg-black rounded-full animate-bounce"></div>
+			</div>
+		{:else}
+			<div class=" mx-auto flex  justify-center items-center space-x-8 mb-5 mt-5">
+				<!-- {#if isLearner === 'ON'} -->
+					<div class="flex flex-col space-y-6 w-1/3 items-center justify-center bg-[#f8f8f8]">
+						<div class="text-center flex flex-col items-center">
+							<div class="flex card shadow-2xl rounded-xl overflow-hidden max-w-4xl">
+								<!-- Photo on the left side of the card -->
+	
+								<!-- Previous Card Content -->
+								<div class="w-full p-8 flex flex-col items-center relative">
+									<!-- <div class="absolute right-2 ">
+										<Switch bind:value={isLearner} fontSize={12} label="Switch to Learner" design="slider" />
+									</div> -->
+									<div class="flex justify-center w-1/2">
+										<img
+											src="https://dxpcgmtdvyvcxbaffqmt.supabase.co/storage/v1/object/public/demo/teacher-removebg-preview.png"
+											alt="Trainer"
+											class="object-contain object-center mb-6"
+										/>
+									</div>
+									<div class="flex flex-col space-y-9">
+										<div class="text-3xl text-center font-bold mb-6">As a Trainer</div>
+									</div>
+									<div>
+										<p class="text-l text-center">
+											Create your own class to teach enthusiast individuals.
+											<br />
+											Set the goals. Maintain the resources. Take Video classroom.
+											<br />Create Question for Community purpose.
+										</p>
+									</div>
+	
+									<div class="w-[100%] mt-5">
+										<button
+											type="submit"
+											class=" btn font-bold text-xl p-4 bg-[#77B8De] rounded-xl shadow-md hover:bg-[#619ecf] hover:text-[21px] hover:shadow-lg w-1/2"
+											on:click={handleSignInTrainer}
+										>
+											Sign In
+										</button>
+									</div>
+	
+									<div class="flex flex-col items-center justify-between mt-3">
+										<p>
+											Don't have account? <a  href="/trainersignup" target="_blank" class="font-medium"><span class="hover-underline-animation">Sign up</span></a>
+										</p>
+									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-			<!-- {:else} -->
-				<div class="flex flex-col space-y-6 w-1/3 items-center justify-center">
-					<div class="text-center flex flex-col items-center">
-						<div class="flex card shadow-2xl rounded-xl overflow-hidden max-w-4xl">
-							<!-- Photo on the left side of the card -->
-
-							<!-- Previous Card Content -->
-							<div class="w-full p-8 flex flex-col items-center relative">
-								<!-- <div class="absolute right-2 "> -->
-									<!-- <span>Switch To Trainer</span> -->
-									<!-- <Switch bind:value={isLearner} fontSize={12} label="Switch to Trainer" design="slider" /> -->
-								<!-- </div> --> 
-								<div class="flex justify-center w-1/2">
-									<img
-										src="https://dxpcgmtdvyvcxbaffqmt.supabase.co/storage/v1/object/public/demo/student.png"
-										alt="Learner"
-										class="object-contain object-center mb-6"
-									/>
-								</div>
-								
-								<div class="flex flex-col space-y-9">
-									<div class="text-3xl text-center font-bold mb-6">As a Learner</div>
-								</div>
-								<div>
-									<p class="text-l text-center">
-										Join the community of the Nerds🔥. Keep up-to-date.
-										<br />
-										Communicate & collaborate. Avail Contents. Help from AI.
-										<br />⚡Storm your Brain with mind-boggling questions🧠
-									</p>
-								</div>
-
-								<div class="w-[100%] mt-5">
-									<button
-										type="submit"
-										class=" btn font-bold text-xl p-4 bg-[#de7692] rounded-xl shadow-md hover:bg-[#b9396c] hover:text-[21px] hover:shadow-lg w-1/2"
-										on:click={handleSignInUser}
-									>
-										Sign In
-									</button>
-								</div>
-
-								<div class="flex flex-col items-center justify-between mt-3">
-									<p>
-										Don't have account?<a href="/learnersignup" target="_blank" class="font-medium"> <span class="hover-underline-animation">Sign up</span> </a>
-										
-									</p>
+				<!-- {:else} -->
+					<div class="flex flex-col space-y-6 w-1/3 items-center justify-center bg-[#f8f8f8]">
+						<div class="text-center flex flex-col items-center">
+							<div class="flex card shadow-2xl rounded-xl overflow-hidden max-w-4xl">
+								<!-- Photo on the left side of the card -->
+	
+								<!-- Previous Card Content -->
+								<div class="w-full p-8 flex flex-col items-center relative">
+									<!-- <div class="absolute right-2 "> -->
+										<!-- <span>Switch To Trainer</span> -->
+										<!-- <Switch bind:value={isLearner} fontSize={12} label="Switch to Trainer" design="slider" /> -->
+									<!-- </div> --> 
+									<div class="flex justify-center w-1/2">
+										<img
+											src="https://dxpcgmtdvyvcxbaffqmt.supabase.co/storage/v1/object/public/demo/student.png"
+											alt="Learner"
+											class="object-contain object-center mb-6"
+										/>
+									</div>
+									
+									<div class="flex flex-col space-y-9">
+										<div class="text-3xl text-center font-bold mb-6">As a Learner</div>
+									</div>
+									<div>
+										<p class="text-l text-center">
+											Join the community of the Nerds🔥. Keep up-to-date.
+											<br />
+											Communicate & collaborate. Avail Contents. Help from AI.
+											<br />⚡Storm your Brain with mind-boggling questions🧠
+										</p>
+									</div>
+	
+									<div class="w-[100%] mt-5">
+										<button
+											type="submit"
+											class=" btn font-bold text-xl p-4 bg-[#CF9261] rounded-xl shadow-md hover:bg-[#e39b5f] hover:text-[21px] hover:shadow-lg w-1/2"
+											on:click={handleSignInUser}
+										>
+											Sign In
+										</button>
+									</div>
+	
+									<div class="flex flex-col items-center justify-between mt-3">
+										<p>
+											Don't have account?<a href="/learnersignup" target="_blank" class="font-medium"> <span class="hover-underline-animation">Sign up</span> </a>
+											
+										</p>
+									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-			<!-- {/if} -->
-		</div>
-		
-		<Footer />
-	{/if}
-</section>
+				<!-- {/if} -->
+			</div>
+			
+			<Footer />
+		{/if}
+	</section>
+</div>
 
 <style>
+	.accessTypeContainer{
+		z-index: -5;
+		aspect-ratio: 960/300;
+		background-repeat: no-repeat;
+		background-position: center;
+		background-size: cover;
+		background-image: url("../../lib/svgs/access-type-bg.svg");
+	}
 	.white-text {
 		color: red;
 	}
--- src/routes/trainerverified/home/recent/+page.svelte
+++ src/routes/trainerverified/home/recent/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-	import { AppRail, AppRailTile, AppRailAnchor } from '@skeletonlabs/skeleton';
+	import { AppRail, AppRailTile, AppRailAnchor, TabGroup,Tab } from '@skeletonlabs/skeleton';
 	import Quill from '$lib/QuillEditor.svelte';
 	import { LightSwitch } from '@skeletonlabs/skeleton';
 	import { popup } from '@skeletonlabs/skeleton';
-	import type { PopupSettings } from '@skeletonlabs/skeleton';
+	import type { PopupSettings, Table } from '@skeletonlabs/skeleton';
 	import { Avatar } from '@skeletonlabs/skeleton';
 	import { InputChip } from '@skeletonlabs/skeleton';
 	import { onMount } from 'svelte';
@@ -25,6 +25,7 @@
 	let hoursLeft: number = 0;
 	let minutesLeft: number = 0;
 	let secondsLeft: number = 0;
+	let searchBarShow: number = 0;
 
 	function calculateCountdown() {
 		const now: Date = new Date();
@@ -57,7 +58,7 @@
 	const popupClick: PopupSettings = {
 		event: 'click',
 		target: 'popupClick',
-		placement: 'top'
+		placement: 'bottom'
 	};
 	const popupHover1: PopupSettings = {
 		event: 'hover',
@@ -80,12 +81,13 @@
 	<div>
 		<div class="navbar">
 			<nav class="appbar">
-				<div class="logo-container">
+				<div class="logo-container flex items-center">
 					<img
 						src="https://dxpcgmtdvyvcxbaffqmt.supabase.co/storage/v1/object/public/demo/GeekGlasses.png"
-						class="transform transition duration-300 hover:rotate-12"
+						class="transform transition duration-300 hover:rotate-12 w-[50px] mr-4"
 						alt="title"
-						width={60}
+						width={50}
+						
 					/>
 					<span class="company-name text-2xl font-extrabold">NerD</span><span
 						class="company-name white-text text-2xl font-extrabold">Herd</span
@@ -155,7 +157,7 @@
 						>
 					</li>
 	
-					<LightSwitch class="mr-3" />
+					<!-- <LightSwitch class="mr-3" /> -->
 					<div use:popup={popupClick}>
 						<Avatar
 							src="https://dxpcgmtdvyvcxbaffqmt.supabase.co/storage/v1/object/public/demo/avro.jpg"
@@ -164,9 +166,9 @@
 						/>
 					</div>
 	
-					<div data-popup="popupClick">
+					<div data-popup="popupClick" class="h-32 absolute">
 						<ul class="text-lg font-semibold bg-sky-300 ml-0">
-							<li class="mt-2 mb-3">
+							<li class="mt-2 mb-3 p-2">
 								<a href="/trainerverified/profile" class="flex items-center font-bold"
 									><img
 										src="https://dxpcgmtdvyvcxbaffqmt.supabase.co/storage/v1/object/public/demo/user-person-profile-block-account-circle-svgrepo-com.svg"
@@ -176,7 +178,7 @@
 									Profile</a
 								>
 							</li>
-							<li class="mb-2">
+							<li class="mb-2 p-2">
 								<a href="/library" class="flex items-center font-bold"
 									><img
 										src="https://dxpcgmtdvyvcxbaffqmt.supabase.co/storage/v1/object/public/demo/logout-svgrepo-com.svg"
@@ -190,119 +192,126 @@
 					</div>
 				</ul>
 			</nav>
-		</div>
+			<TabGroup class="w-full h-14 bg-[#e6f5ff]  mt-2 flex justify-center">
+				<Tab bind:group={currentTile} name="tab1" value={0} class="hover:scale-105 hover:bg-[#c8e4f7]">
 		
-		<!-- <TrainerNavBar></TrainerNavBar> -->
-	</div>
-	<div class="w-full h-screen flex flex-row mt-[80px]">
-		<div>
-			<AppRail class="w-fit overflow-hidden ">
-				<AppRailTile bind:group={currentTile} name="tile-1" value={0} title="tile-1">
+					<!-- <svelte:fragment slot="lead">(icon)</svelte:fragment>
+					<span>(label 1)</span>
+			 -->
 					<a
 						href="/trainerverified/home/recent"
-						class="flex flex-col items-center justify-center p-3 font-bold"
+						class="flex items-center justify-center p-2 font-semibold"
 						><img
 							src="https://dxpcgmtdvyvcxbaffqmt.supabase.co/storage/v1/object/public/demo/clock-svgrepo-com.svg"
 							alt="Dashboard Icon"
-							class="h-10 w-10 hover:rotate-12 hover:scale-105"
+							class="h-6 w-6 m-1  hover:scale-105 hover:bg-[#c8e4f7]"
 						/>
 						Recent
 					</a>
-				</AppRailTile>
-				<AppRailTile bind:group={currentTile} name="tile-2" value={1} title="tile-2">
+				</Tab>
+				<Tab bind:group={currentTile} name="tab2" value={1} class="hover:scale-105 hover:bg-[#c8e4f7]">
 					<a
 						href="/trainerverified/home/my"
-						class="flex flex-col items-center justify-center p-3 font-bold"
+						class="flex  items-center justify-center p-2 font-semibold"
 						><img
 							src="https://dxpcgmtdvyvcxbaffqmt.supabase.co/storage/v1/object/public/demo/personal-account-account-svgrepo-com.svg"
 							alt="Dashboard Icon"
-							class="h-10 w-10 hover:rotate-12 hover:scale-105"
+							class="h-6 w-6 m-1  hover:scale-105 hover:bg-[#c8e4f7]"
 						/>
 						My Articles
 					</a>
-				</AppRailTile>
-				<AppRailTile bind:group={currentTile} name="tile-3" value={2} title="tile-3">
+				</Tab>
+				<Tab bind:group={currentTile} name="tab3" value={2} class="hover:scale-105 hover:bg-[#c8e4f7]">
 					<a
 						href="/trainerverified/home/popular"
-						class="flex flex-col items-center justify-center p-3 font-bold"
+						class="flex  items-center justify-center p-2 font-semibold"
 						><img
 							src="https://dxpcgmtdvyvcxbaffqmt.supabase.co/storage/v1/object/public/demo/fire-svgrepo-com.svg"
 							alt="Dashboard Icon"
-							class="h-10 w-10 hover:rotate-12 hover:scale-105"
+							class="h-6 w-6 m-1   hover:scale-105 hover:bg-[#c8e4f7]"
 						/>
 						Popular
 					</a>
-				</AppRailTile>
-	
-				<AppRailTile bind:group={currentTile} name="tile-5" value={4} title="tile-5">
+				</Tab>
+				<Tab bind:group={currentTile} name="tab3" value={3} class="hover:scale-105 hover:bg-[#c8e4f7]">
 					<a
 						href="/trainerverified/home/saved"
-						class="flex flex-col items-center justify-center p-3 font-bold"
+						class="flex items-center justify-center p-2 font-semibold"
 						><img
 							src="https://dxpcgmtdvyvcxbaffqmt.supabase.co/storage/v1/object/public/demo/save-save-the-document-svgrepo-com.svg"
 							alt="Dashboard Icon"
-							class="h-10 w-10 hover:rotate-12 hover:scale-105"
+							class="h-6 w-6 m-1"
 						/>
 						Saved
 					</a>
-				</AppRailTile>
-	
-				<!-- --- -->
-				<svelte:fragment slot="trail">
-					<div class="mb-24 flex flex-col items-center justify-center" on:click={handleSignOut}>
+				</Tab>
+				<Tab bind:group={currentTile} name="tab3" value={4} class="hover:scale-105 hover:bg-[#c8e4f7]">
+					<button class="flex items-center justify-center p-2 font-semibold" on:click={()=> searchBarShow = searchBarShow^1}>
 						<img
-							src="https://dxpcgmtdvyvcxbaffqmt.supabase.co/storage/v1/object/public/demo/power-button-svgrepo-com.svg"
-							alt="Dashboard Icon"
-							class="h-10 w-10 hover:rotate-12 hover:scale-105"
-						/>
-						Logout
-					</div>
-				</svelte:fragment>
-			</AppRail>
-		</div>
-		<div class="p-6">
-			<div class=" flex flex-row mb-10">
-				<div class="flex flex-row">
-					<div class="md:w-1/3 lg:w-1/3 mr-3">
-						<label class="label">
-							<span class="font-semibold">Search By Title</span>
-							<input class="input" type="text" placeholder="Enter Name of the Article" />
-						</label>
-					</div>
-					<div class="md:w-1/3 lg:w-1/3 mr-3">
-						<label class="label">
-							<span class="font-semibold">Search By Author</span>
-							<input class="input" type="text" placeholder="Enter Name of the Author" />
-						</label>
-					</div>
-					<div class=" md:w-1/3 lg:w-1/3 mr-3">
-						<label class="label">
-							<span class="font-semibold">Search By Tag</span>
-							<InputChip bind:value={list} name="chips" placeholder="Enter Tags..." />
-						</label>
-					</div>
-				</div>
-				<div class="w-1/6 mt-6 border-2 h-11">
-					<button
-						class=" btn font-bold text-[18px] py-4 bg-[#45d588] rounded-sm shadow-md hover:bg-[#15b141] hover:text-[19px] hover:shadow-lg w-[100%] h-[100%]"
-					>
-						<img
-							src="https://aaitclybvvendvuswytq.supabase.co/storage/v1/object/public/BDeHR/search-new.svg"
-							class="transform transition duration-300 hover:rotate-12 hover:scale-110"
-							alt="Search Icon"
-							width="20"
-							height="20"
-							style="vertical-align: middle;"
-						/>
-						Search
+								src="https://aaitclybvvendvuswytq.supabase.co/storage/v1/object/public/BDeHR/search-new.svg"
+								class="transform transition duration-300 hover:rotate-12 hover:scale-110 h-6 w-6 m-1"
+								alt="Search Icon"
+								style="vertical-align: middle;"
+							/>
+							Search
 					</button>
+				</Tab>
+				<!-- Tab Panels --->
+				<svelte:fragment slot="panel">
+					
+				</svelte:fragment>
+			</TabGroup>
+		
+			<div class={searchBarShow?"p-2 bg-slate-200":"hidden p-2"}>
+				<div class=" flex flex-row mb-1 justify-center">
+					<div class="flex flex-row ">
+						<div class="md:w-1/3 lg:w-1/3 mr-3">
+							<label class="label">
+								<span class="pl-1">Search By Title</span>
+								<input class="input rounded-xl border-[1px] bg-slate-200" type="text" placeholder="Enter Name of the Article" />
+							</label>
+						</div>
+						<div class="md:w-1/3 lg:w-1/3 mr-3">
+							<label class="label">
+								<span class="pl-1">Search By Author</span>
+								<input class="input rounded-xl border-[1px] bg-slate-200" type="text" placeholder="Enter Name of the Author" />
+							</label>
+						</div>
+						<div class=" md:w-1/3 lg:w-1/3 mr-3">
+							<label class="label ">
+								<span class="">Search By Tag</span>
+								<InputChip bind:value={list} class="input rounded-full border-[1px] bg-slate-200" name="chips" placeholder="Enter Tags..." />
+							</label>
+						</div>
+					</div>
+					<div class="w-1/6 mt-6 border-2 h-11">
+						<button
+							class=" btn font-bold text-[18px] py-4 bg-[#45d588] rounded-sm shadow-md hover:bg-[#15b141] hover:text-[19px] hover:shadow-lg w-[100%] h-[100%]"
+						>
+							<img
+								src="https://aaitclybvvendvuswytq.supabase.co/storage/v1/object/public/BDeHR/search-new.svg"
+								class="transform transition duration-300 hover:rotate-12 hover:scale-110"
+								alt="Search Icon"
+								width="20"
+								height="20"
+								style="vertical-align: middle;"
+							/>
+							Search
+						</button>
+					</div>
 				</div>
 			</div>
-			<div class="grid grid-cols-3 mt-6 p-6 w-full">
+		</div>
+	</div>
+	
+	<div class="w-full h-screen flex flex-row mt-[120px] justify-center">
+		<div class="w-4/5">
+			
+			<div class="grid grid-cols-4 mt-6 p-6 w-full">
 				{#each blog as currblog, i}
 					<div class="m-3 overflow-hidden bg-white hover:bg-[#efeded] rounded-md shadow-2xl pb-3 cursor-pointer ">
 						<div class="hover:scale-105">
-							<div class="mb-3 ">
+							<div class="mb-3 p-3 rounded-full">
 								<img
 									src="https://skawbthbjfypoflcurec.supabase.co/storage/v1/object/public/avro012/thumbnail.jpg"
 									alt="User "
@@ -364,16 +373,19 @@
 
 	
 
-	.logo-container img {
-		width: 50px;
-		margin-right: 1rem;
+	.links {
+		display: flex;
+		list-style: none;
+		margin: 0;
 	}
-
+	
 	.links li {
 		margin-left: 2rem;
 	}
-
-	
+	.links a {
+		text-decoration: none;
+		transition: color 0.2s ease-in-out;
+	}
 
 	.links a:hover {
 		color: #007bff; /* Accent color from Skeleton UI */
@@ -382,13 +394,15 @@
 		color: red;
 	}
 	.company-name {
+		border: 1px solid black;
 		font-size: 2rem; /* Adjust font size as needed */
-		margin-top: 1rem; /* Add spacing if necessary */
+		margin-top: .5rem; /* Add spacing if necessary */
 		font-family: 'CustomFont', sans-serif; /* Use your custom font */
+		
 	}
     .navbar{
         overflow: hidden;
-        position: fixed; /* Set the navbar to fixed position */
+        position: absolute; /* Set the navbar to fixed position */
         top: 0; /* Position the navbar at the top of the page */
         width: 100%; /* Full width */
         z-index: 5;
@@ -402,18 +416,6 @@
 	}
 
 
-	.links {
-		display: flex;
-		list-style: none;
-		margin: 0;
-	}
-
-
-	.links a {
-		text-decoration: none;
-
-		transition: color 0.2s ease-in-out;
-	}
-
+	
 	
 </style>
