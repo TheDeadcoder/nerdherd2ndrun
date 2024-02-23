aebdfd3940450d269f13139a1cbd0470255bc705
TheDeadcoder <sakibbuet2k19@gmail.com>
2024-02-23 21:54:15 +0600

    live class via registration. Next - attendence and others

--- src/routes/trainerverified/runningclass/[classid]/live/+page.server.ts
+++ src/routes/trainerverified/runningclass/[classid]/live/+page.server.ts
@@ -3,6 +3,7 @@ import { error } from '@sveltejs/kit';
 import type { Actions } from './$types';
 
 let classNow;
+let teacherNow;
 
 export const load = async ({ params, locals: { supabase, getSession } }) => {
     console.log(params.classid);
@@ -11,6 +12,17 @@ export const load = async ({ params, locals: { supabase, getSession } }) => {
     if (!session) {
         throw redirect(303, '/')
     }
+    const {
+        data: { user }
+    } = await supabase.auth.getUser();
+    //console.log(user);
+
+
+    let { data: teacher, error: err } = await supabase
+        .from('teacher')
+        .select("*")
+        .eq('email', user.email)
+    teacherNow = teacher[0];
 
 
     let { data: classes, error: err1 } = await supabase
@@ -21,12 +33,47 @@ export const load = async ({ params, locals: { supabase, getSession } }) => {
     classNow = classes[0];
 
 
+    let { data: classlive, error: err3 } = await supabase
+        .from('classlive')
+        .select("*")
+        .eq('classid', classNow.id)
+
+
+
     let { data: studclass, error: err2 } = await supabase
         .from('studclass')
         .select("*")
         .eq('cid', classNow.id)
         .eq('joined', true)
 
-    return { classNow, studclass };
+    return { classNow, studclass, teacherNow, classlive };
+
+}
+export const actions = {
+
+    addLive: async ({ request, locals: { supabase, getSession } }) => {
+        const data = await request.formData();
+        //console.log("amar add class form holo", data);
+
+        let newClass = Object.fromEntries(data.entries()) as any;
+
+        //console.log(newClass.title, newClass.syllabus, newClass.start, newClass.duration, name)
+
+
+        const { data: dtt, error: err1 } = await supabase
+            .from('classlive')
+            .insert([
+                { classid: classNow.id, start: newClass.start, topic: newClass.topic },
+            ])
+            .select()
+
+        if (err1) console.log(err1)
+
+
+
+        throw redirect(303, `/trainerverified/runningclass/${classNow.id}/live`);
+    },
+
+
 
 }
--- src/routes/trainerverified/runningclass/[classid]/live/+page.svelte
+++ src/routes/trainerverified/runningclass/[classid]/live/+page.svelte
@@ -1,25 +1,73 @@
 <script lang="ts">
 	export let data;
-	import { onMount } from 'svelte';
+	import { enhance } from '$app/forms';
+	import { onMount, onDestroy } from 'svelte';
 	import { page } from '$app/stores';
 	import { Avatar } from '@skeletonlabs/skeleton';
 	const { classid } = $page.params;
+	let { session, supabase, classNow, studclass, teacherNow, classlive } = data;
+	$: ({ session, supabase, classNow, studclass, teacherNow, classlive } = data);
 
 	let uid = 100;
-	const handleClick = () => {
-		window.location.href = `/call/${classid}?uid=${uid}`;
-	};
-
-	let { session, supabase, classNow, studclass } = data;
-	$: ({ session, supabase, classNow, studclass } = data);
 
 	let isSidebarOpen = false;
+
+	let start;
+	let topic;
+
+	let addliveModal = false;
+	function openliveModal() {
+		addliveModal = true;
+	}
+
+	function closeliveModal() {
+		addliveModal = false;
+	}
+
+	function goLive(val) {
+		window.location.href = `/commonverified/call/${val}?uid=${teacherNow.id}`;
+	}
+
+	function calculateCountdown(endTime) {
+		const total = Date.parse(endTime) - Date.parse(new Date());
+		const seconds = Math.floor((total / 1000) % 60);
+		const minutes = Math.floor((total / 1000 / 60) % 60);
+		const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+		const days = Math.floor(total / (1000 * 60 * 60 * 24));
+		return {
+			total,
+			days,
+			hours,
+			minutes,
+			seconds
+		};
+	}
+
+	// Function to update countdown for each classLive
+	function updateCountdown() {
+		classlive = classlive.map((classLive) => {
+			if (new Date(classLive.start) > new Date()) {
+				const countdown = calculateCountdown(classLive.start);
+				return { ...classLive, countdown };
+			} else {
+				let countdown;
+				return { ...classLive, countdown };
+			}
+			return classLive;
+		});
+	}
+
 	onMount(() => {
+		const interval = setInterval(updateCountdown, 1000);
+		updateCountdown();
 		function handleOutsideClick(event) {
 			if (!event.target.closest('.sidebar')) {
 				isSidebarOpen = false;
 			}
 		}
+		onDestroy(() => {
+			clearInterval(interval);
+		});
 		document.addEventListener('click', handleOutsideClick);
 		return () => document.removeEventListener('click', handleOutsideClick);
 	});
@@ -94,63 +142,147 @@
 		</li>
 	</ul>
 </nav>
-<main class="min-h-screen flex overflow-auto">
+<main class="min-h-screen grid grid-cols-[15%_auto]">
 	<div
-		class={`transform top-0 left-0 w-64 bg-white  fixed min-h-screen overflow-auto ease-in-out transition-all duration-300 z-30 ${
+		class={`transform bg-[#c2d5e7] text-black   min-h-screen overflow-auto ease-in-out transition-all duration-300 z-30 ${
 			isSidebarOpen ? 'translate-x-0' : '-translate-x-full'
 		} lg:translate-x-0 lg:static lg:block`}
 	>
-		<div class="flex flex-col p-4 mt-4">
-			<a href="/trainerverified/runningclass/{classid}/chat" class="sidebar-item mb-2">
-				<div class="flex flex-row">
-					<img
-						src="https://dxpcgmtdvyvcxbaffqmt.supabase.co/storage/v1/object/public/demo/chat-svgrepo-com.svg"
-						alt="Dashboard Icon"
-						class="h-6 mr-3 hover:rotate-12"
-					/>
-					Chat
-				</div>
-			</a>
-			<a href="/trainerverified/runningclass/{classid}/assignments" class="sidebar-item mb-2">
-				<div class="flex flex-row">
-					<img
-						src="https://dxpcgmtdvyvcxbaffqmt.supabase.co/storage/v1/object/public/demo/bell-svgrepo-com.svg"
-						alt="Dashboard Icon"
-						class="h-6 mr-3 hover:rotate-12"
-					/>
-					Assignments
-				</div>
-			</a>
-			<a href="/trainerverified/runningclass/{classid}/credentials" class="sidebar-item mb-2">
-				<div class="flex flex-row">
-					<img
-						src="https://dxpcgmtdvyvcxbaffqmt.supabase.co/storage/v1/object/public/demo/certify-svgrepo-com.svg"
-						alt="Dashboard Icon"
-						class="h-6 mr-3 hover:rotate-12"
-					/>
-					Credentials
-				</div>
-			</a>
-			<a href="/trainerverified/runningclass/{classid}/live" class="sidebar-item active mb-2">
-				<div class="flex flex-row">
-					<img
-						src="https://dxpcgmtdvyvcxbaffqmt.supabase.co/storage/v1/object/public/demo/conference-live-video-svgrepo-com.svg?t=2024-02-09T19%3A53%3A53.776Z"
-						alt="Dashboard Icon"
-						class="h-6 mr-3 hover:rotate-12"
-					/>
-					Go Live
-				</div>
-			</a>
+		<div class="flex flex-col mt-6">
+			<div>
+				<a href="/trainerverified/runningclass/{classid}/chat" class="sidebar-item">
+					<div class="flex flex-row">
+						<img
+							src="https://dxpcgmtdvyvcxbaffqmt.supabase.co/storage/v1/object/public/demo/chat-svgrepo-com.svg"
+							alt="Dashboard Icon"
+							class="h-6 mr-3 hover:rotate-12"
+						/>
+						<p class="text-black font-bold">Chat</p>
+					</div>
+				</a>
+			</div>
+			<div>
+				<a href="/trainerverified/runningclass/{classid}/assignments" class="sidebar-item">
+					<div class="flex flex-row">
+						<img
+							src="https://dxpcgmtdvyvcxbaffqmt.supabase.co/storage/v1/object/public/demo/bell-svgrepo-com.svg"
+							alt="Dashboard Icon"
+							class="h-6 mr-3 hover:rotate-12"
+						/>
+						<p class="text-black font-bold">Assignments</p>
+					</div>
+				</a>
+			</div>
+			<div>
+				<a href="/trainerverified/runningclass/{classid}/credentials" class="sidebar-item">
+					<div class="flex flex-row">
+						<img
+							src="https://dxpcgmtdvyvcxbaffqmt.supabase.co/storage/v1/object/public/demo/certify-svgrepo-com.svg"
+							alt="Dashboard Icon"
+							class="h-6 mr-3 hover:rotate-12"
+						/>
+						<p class="text-black font-bold">Credentials</p>
+					</div>
+				</a>
+			</div>
+			<div>
+				<a href="/trainerverified/runningclass/{classid}/live" class="sidebar-item active">
+					<div class="flex flex-row">
+						<img
+							src="https://dxpcgmtdvyvcxbaffqmt.supabase.co/storage/v1/object/public/demo/conference-live-video-svgrepo-com.svg?t=2024-02-09T19%3A53%3A53.776Z"
+							alt="Dashboard Icon"
+							class="h-6 mr-3 hover:rotate-12"
+						/>
+						<p class="text-black font-bold">Go Live</p>
+					</div>
+				</a>
+			</div>
 		</div>
 	</div>
 	<div class={`p-10 ${isSidebarOpen ? 'flex-1 p-6 flex-grow' : ''}`}>
-		<h1>Start Class</h1>
-		<div class="text-column">
-			<button class="btn bg-blue-300" on:click={handleClick}>Go Live</button>
+		<button
+			class="btn bg-emerald-400 mt-6 p-4 rounded-md flex flex-row space-x-2"
+			on:click={openliveModal}
+		>
+			<img
+				src="https://rxkhdqhbxkogcnbfvquu.supabase.co/storage/v1/object/public/statics/clock-svgrepo-com.svg"
+				alt="Dashboard Icon"
+				class="h-6 hover:rotate-12"
+			/>
+			<h4 class="font-semibold">Schedule a new Class</h4>
+		</button>
+
+		<div class="mt-9 flex flex-col space-y-4">
+			<h1 class="text-2xl font-extrabold"><u>All Classes</u></h1>
+			{#each classlive as currsession, i}
+				<div class=" mr-40 flex flex-row space-x-8">
+					<h1 class="font-bold text-xl">
+						{i + 1}. {currsession.topic}
+					</h1>
+					{#if currsession.done}
+						<button class="btn bg-teal-500"> Check Stat </button>
+					{:else if currsession.countdown}
+						<p class="font-semibold">
+							{currsession.countdown.days}d : {currsession.countdown.hours}h : {currsession
+								.countdown.minutes}m : {currsession.countdown.seconds}s
+						</p>
+					{:else}
+						<button
+							class="bg-blue-500 text-white px-4 py-2 rounded"
+							on:click={() => goLive(currsession.id)}>Go Live</button
+						>
+					{/if}
+				</div>
+			{/each}
 		</div>
-		<pre>{JSON.stringify(classNow, null, 2)}</pre>
+
+		<!-- <pre>{JSON.stringify(classNow, null, 2)}</pre>
 		<pre>{JSON.stringify(studclass, null, 2)}</pre>
+		<pre>{JSON.stringify(classlive, null, 2)}</pre> -->
 	</div>
+	{#if addliveModal}
+		<div
+			class="fixed inset-0 bg-opacity-50 flex justify-center items-center z-50 transition-opacity backdrop-blur-sm"
+		>
+			<div class="bg-blue-200 p-6 rounded-lg shadow-lg max-w-md w-full m-4">
+				<div class="flex justify-between items-center mb-4">
+					<h2 class="text-2xl font-bold">Schedule for a new Class</h2>
+					<button class=" text-lg" on:click={closeliveModal}>&times;</button>
+				</div>
+
+				<form
+					use:enhance
+					action="?/addLive"
+					method="POST"
+					on:submit={() => {
+						closeliveModal();
+					}}
+				>
+					<div class="flex flex-col space-y-6">
+						<label class="label text-left mb-3">
+							<span>Topic to be Covered</span>
+
+							<input class="input" type="text" id="topic" name="topic" bind:value={topic} />
+						</label>
+						<label class="label text-left mb-3">
+							<span>Starting time</span>
+
+							<input
+								class="input"
+								type="datetime-local"
+								id="start"
+								name="start"
+								bind:value={start}
+							/>
+						</label>
+						<button type="submit" class="btn variant-filled-primary text-xl font-semibold">
+							Submit
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	{/if}
 </main>
 
 <style>
